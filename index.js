18c30d89416c8fb2ee8807e9d8366b063dcc6c5f
Артём Цыганов <tsiganovartem06@gmail.com>
2026-03-03 18:12:38 +0500

    3&4

--- index.js
+++ index.js
@@ -24,8 +24,46 @@ function findAllTodos(files) {
     return todos;
 }
 
+function findAllImportantTodos(files) {
+    const todos = [];
+    for (const file of files) {
+        const lines = file.split('\r\n');
+        for (const line of lines) {
+            if (/\/\/ TODO.*!.*/.test(line)) {
+                todos.push(line);
+            }
+        }
+    }
+    return todos;
+}
+
+
+function findAllTodosFromUser(files, username) {
+    const todos = [];
+    for (const file of files) {
+        const lines = file.split('\r\n');
+        for (const line of lines) {
+
+            const match = line.match(/\/\/\s*TODO.*$/);
+            if (match) {
+                todoLine = match[0];
+                let matchUsername = todoLine.match(new RegExp(`${username};\s*(.+;)(.+)`, 'i'));
+                // console.log(a);
+                if (matchUsername) {
+                    todos.push(line);
+                }
+            }
+        }
+    }
+    return todos;
+}
+
+
 function processCommand(command) {
-    switch (command) {
+    command = command.trim();
+    core = command.split(' ')[0];
+    args = command.split(' ').slice(1);
+    switch (core) {
         case 'exit':
             process.exit(0);
             break;
@@ -34,7 +72,19 @@ function processCommand(command) {
                 console.log(`${index + 1}: ${todo.trim()}\n`);
             }
             break;
+        case 'important':
+            for (const [index, todo] of findAllImportantTodos(files).entries()) {
+                console.log(`${index + 1}: ${todo.trim()}\n`);
+            }
+            break;
+        case 'user':
+            const username = args[0];
+            for (const [index, todo] of findAllTodosFromUser(files, username).entries()) {
+                console.log(`${index + 1}: ${todo.trim()}\n`);
+            }
+            break;
         default:
+            console.log(command + "\n");
             console.log('wrong command');
             break;
     }
