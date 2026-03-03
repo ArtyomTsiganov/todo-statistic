523755c3008d4e8e8e8791298228e17dc7818553
Артём Цыганов <tsiganovartem06@gmail.com>
2026-03-03 17:32:24 +0500

    1&2

--- index.js
+++ index.js
@@ -11,10 +11,28 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function findAllTodos(files) {
+    const todos = [];
+    for (const file of files) {
+        const lines = file.split('\r\n');
+        for (const line of lines) {
+            if (line.includes('// TODO')) {
+                todos.push(line);
+            }
+        }
+    }
+    return todos;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            for (const [index, todo] of findAllTodos(files).entries()) {
+                console.log(`${index + 1}: ${todo.trim()}\n`);
+            }
             break;
         default:
             console.log('wrong command');
